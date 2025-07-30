5da1a55434849d61af35529da00cafd0838c22b1
maria-baixinha <141179600+maria-baixinha@users.noreply.github.com>
2025-07-30 13:45:28 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ function setup() {
 
 function draw() {
   background((0,100,0)); //cor de fundo
-  rotateY(millis()/1000); // rotação da terra
+  rotateY(millis()/1400); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(380, 380); //esfera 3D
+  sphere(350, 350); //esfera 3D
   
   
 }
